be7d801d3a43b86d6c02ecc2d40b6e4c1b34d7cd
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-13 18:35:17 -0500

    Add a few watch scripts

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const Koa = require("koa");
 const app = (module.exports = new Koa());
 
 app.use(async ctx => {
+    console.log("restart")
     ctx.body = "Howdy from Koa!";
 });
 
 const port = process.env.PORT || (process.argv[2] || 4000);
 
-app.listen(port);
+if (!module.parent) { app.listen(port); }
 console.log(`Application started. Listening on port: ${port}`);
