98d1212271d7b5daae89b372927b28ad13bd20dd
Jon Crowell <jon.crowell@stonegiantstudio.com>
2019-08-13 17:16:00 -0500

    Prettier formatting changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Koa = require('koa');
-const app = module.exports = new Koa();
+const Koa = require("koa");
+const app = (module.exports = new Koa());
 
 app.use(async ctx => {
-    ctx.body = "Howdy from Koa!"
-})
+    ctx.body = "Howdy from Koa!";
+});
 
-const port = process.env.PORT || (process.argv[2] || 4000)
+const port = process.env.PORT || (process.argv[2] || 4000);
 
-app.listen(port)
-console.log(`Application started. Listening on port: ${port}`)
+app.listen(port);
+console.log(`Application started. Listening on port: ${port}`);
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const should = require("should");
 
-describe("A very simple test", function () {
-    it("is running now", function () {
-        true.should.equal(true);
-    })
-})
+describe("A very simple test", function() {
+  it("is running now", function() {
+    true.should.equal(true);
+  });
+});
